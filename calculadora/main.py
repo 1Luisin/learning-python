a731cf22d1140f53b9f990eef3e194a16c209de4
Luis Guilherme <guilherme32698@gmail.com>
2026-02-11 17:49:26 -0300

    calculadora/main.py aplicando conhecimento de manipulação winddows na calculadora

--- calculadora/main.py
+++ calculadora/main.py
@@ -1,3 +1,9 @@
+import os
+
+def limparConsole():
+    comando = "cls" if os.name == 'nt' else "clear"
+    os.system(comando)
+
 def funcaoPrincipal():
     
     opcaoEscolhida = int(input("Digite qual modo você deseja:" + "\n1 - Soma"+ "\n2 - Subtração"+ "\n3 - Multiplicação"+ "\n4 - Divisão\n"))
@@ -11,9 +17,11 @@ def funcaoPrincipal():
     elif(opcaoEscolhida == 4):
         funcaoDivisao()
     else:
+        limparConsole()
         print("Opção não encontrada, tente novamente.")
         funcaoPrincipal()
-
+        
+        
 def funcaoSoma():
     input1 = int(input("Digite o primeiro número\n"))
     input2 = int(input("Digite o segundo número\n"))
@@ -21,6 +29,9 @@ def funcaoSoma():
     resultado = input1 + input2
 
     print("O resultado da soma entre os dois números é " + str(resultado) + "." )
+    input("Pressione qualquer tecla para continuar. . .")
+    limparConsole()
+    funcaoPrincipal()
 
 
 def funcaoSubtracao():
@@ -30,6 +41,10 @@ def funcaoSubtracao():
     resultado = input1 - input2
 
     print("O resultado da soma entre os dois números é " + str(resultado) + "." )
+    input("Pressione qualquer tecla para continuar. . .")
+    limparConsole()
+    funcaoPrincipal()
+
 
 def funcaoMultiplicacao():
     input1 = int(input("Digite o primeiro número\n"))
@@ -38,6 +53,9 @@ def funcaoMultiplicacao():
     resultado = input1 * input2
 
     print("O resultado da soma entre os dois números é " + str(resultado) + "." )
+    input("Pressione qualquer tecla para continuar. . .")
+    limparConsole()
+    funcaoPrincipal()
 
 
 def funcaoDivisao():
@@ -47,7 +65,10 @@ def funcaoDivisao():
     resultado = input1 / input2
 
     print("O resultado da soma entre os dois números é " + str(resultado) + "." )
+    input("Pressione qualquer tecla para continuar. . .")
+    limparConsole()
+    funcaoPrincipal()
 
-
+limparConsole()
 funcaoPrincipal()
 
