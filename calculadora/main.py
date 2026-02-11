b2574ea27a0f6cb62bc6945b0257f9cf169907f8
Luis Guilherme <guilherme32698@gmail.com>
2026-02-11 17:16:29 -0300

    calculadora\main.py função principal criada

---
+++ calculadora/main.py
@@ -0,0 +1,5 @@
+def funcaoPrincipal():
+    
+    opcaoEscolhida = int(input("Digite qual modo você deseja:" + "\n1 - Soma"+ "\n2 - Subtração"+ "\n3 - Multiplicação"+ "\n4 - Divisão"))
+    
+        
