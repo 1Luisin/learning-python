5bcbe2e151e80de110e494eeb5c1bde2f66a2665
Luis Guilherme <guilherme32698@gmail.com>
2026-02-13 18:55:23 -0300

    exercicio1 modificado

--- Exercícios/Exercicio1.py
+++ Exercícios/Exercicio1.py
@@ -1,7 +1,7 @@
 # Questão 1) Faça um procedimento que receba por parâmetro um número inteiro e escreva “par” se o número recebido por parâmetro for par. Caso contrário, o procedimento deve escrever “ímpar”. No método principal (Main), leia um número inteiro e passe-o por parâmetro para o procedimento criado.
 
-ímpar = 0
-par = 1
+par = 0
+ímpar = 1
 
 def procedimento(numeroInteiro):
     if(numeroInteiro % 2 == 0):
