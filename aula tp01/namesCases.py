85d1b7eadca0c8265ac6f0f2e24ae0e207aafacc
Luis Guilherme <guilherme32698@gmail.com>
2026-02-23 21:52:02 -0300

    aprendendo a manipular string

---
+++ aula tp01/namesCases.py
@@ -0,0 +1,11 @@
+#exercício 1
+
+nome = input("Bem vindo, qual é seu nome?\n")
+print(f"Olá {nome.title()}, gostaria de aprender Python hoje?")
+
+#exercicio2
+
+nome2 = (input("Digite seu nome:\n"))
+print(nome2.lower())
+print(nome2.upper())
+print(nome2.title())
